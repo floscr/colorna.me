282fc061fb44487f5f2249c005c4e628ae743ba0
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-01-03 23:50:15 +0100

    Remove vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import store from 'src/store'
 import App from 'src/App'
 
 new Vue({
   el: '#app',
-  store,
   render: h => h(App),
 })
