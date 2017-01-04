5a962e782d034d5b87cc18338547de32849ebce0
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-01-04 10:18:55 +0100

    Add github-corner

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,24 @@
 <template>
   <div class="container">
     <color-name-picker></color-name-picker>
+    <github-corner
+      cornerColor="#c2cbce"
+      gitColor="#EFF3F3"
+      :size="60"
+      url="https://github.com/floscr/colorna.me">
+    </github-corner>
   </div>
 </template>
 
 <script>
   import ColorNamePicker from './components/ColorNamePicker'
+  import GithubCorner from 'vue-github-corners'
 
   export default {
-
     components: {
       ColorNamePicker,
+      GithubCorner,
     },
-
   }
 </script>
 
