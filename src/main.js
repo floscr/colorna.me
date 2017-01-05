dff3116a86fbc2e605561c18cd696734ff1f3eb5
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-01-05 11:33:39 +0100

    Replace package with simple tracking script in footer

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from 'src/App'
 
-// Google Analytics
-import VueAnalytics, { loadScript } from 'vue-analytics'
-Vue.use(VueAnalytics)
-loadScript('UA-89852478-1')
-
 // Main App init and at to dom
 new Vue({
   el: '#app',
