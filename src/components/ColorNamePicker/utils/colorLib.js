e157c4154f45ea77ebdaca6f6f293c516889726e
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-01-09 20:36:58 +0100

    Reduce giant header comment

--- src/components/ColorNamePicker/utils/colorLib.js
+++ src/components/ColorNamePicker/utils/colorLib.js
@@ -1,34 +1,8 @@
 /* eslint-disable */
 /*
-
-+-----------------------------------------------------------------+
-|     Created by Chirag Mehta - http://chir.ag/projects/ntc       |
-|-----------------------------------------------------------------|
-|               ntc js (Name that Color JavaScript)               |
-+-----------------------------------------------------------------+
-
-All the functions, code, lists etc. have been written specifically
-for the Name that Color JavaScript by Chirag Mehta unless otherwise
-specified.
-
-This script is released under the: Creative Commons License:
-Attribution 2.5 http://creativecommons.org/licenses/by/2.5/
-
-Sample Usage:
-
-  <script type="text/javascript" src="ntc.js"></script>
-
-  <script type="text/javascript">
-
-    var n_match  = ntc.name("#6195ED");
-    n_rgb        = n_match[0]; // This is the RGB value of the closest matching color
-    n_name       = n_match[1]; // This is the text string for the name of the match
-    n_exactmatch = n_match[2]; // True if exact color match, False if close-match
-
-    alert(n_match);
-
-  </script>
-
+n_match[0]; // This is the RGB value of the closest matching color
+n_match[1]; // This is the text string for the name of the match
+n_match[2]; // True if exact color match, False if close-match
 */
 
 var ntc = {
