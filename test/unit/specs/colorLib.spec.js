3dd2ce34bd03601109f6440bf9c0eb55fb80259d
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-01-09 20:36:10 +0100

    Remove init method, expect color in return

--- test/unit/specs/colorLib.spec.js
+++ test/unit/specs/colorLib.spec.js
@@ -1,10 +1,13 @@
-import colorLib from 'src/components/ColorNamePicker/utils/colorLib.js'
+import { name } from 'src/components/ColorNamePicker/utils/colorLib.js'
 
 describe('colorLib', () => {
 
-  it('It should return the color name matching the nearest Hex Color', () => {
-    colorLib.init()
-    expect(colorLib.name('#FFFFFF')[1]).to.equal('White')
+  it('#FFFFFF should return white', () => {
+    expect(name('FFFFFF')).to.equal('Black')
+  })
+
+  it('#000000 should return black', () => {
+    expect(name('000000')).to.equal('Black')
   })
 
 })
